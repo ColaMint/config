c9e29de63cc83c2e9a6a80a6b3628f81e135e9b6
Mint <463785757@qq.com>
2023-03-16 20:22:12 +0800

    noice

--- init.lua
+++ init.lua
@@ -843,6 +843,22 @@ require("lazy").setup({
             })
         end,
     },
+    -- {
+    --     "JuanZoran/Trans.nvim",
+    --     branch = "dev",
+    --     build = ":lua require('Trans').install()",
+    --     keys = {
+    --         { "mm", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = " Translate" },
+    --         { "mk", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " 自动发音" },
+    --         { "mi", "<Cmd>TranslateInput<CR>", desc = " Translate From Input" },
+    --     },
+    --     dependencies = { "kkharji/sqlite.lua", lazy = true },
+    --     config = function()
+    --         require("Trans").setup({
+    --             dir = os.getenv("HOME") .. "/.local/share/nvim/Trans.nvim",
+    --         })
+    --     end,
+    -- },
     {
         "romgrk/barbar.nvim",
         dependencies = "nvim-tree/nvim-web-devicons",
@@ -887,6 +903,7 @@ require("lazy").setup({
         "rcarriga/nvim-notify",
         config = function()
             vim.opt.termguicolors = true
+            vim.notify = require("notify")
         end,
     },
     {
@@ -1006,4 +1023,14 @@ require("lazy").setup({
     --         vim.cmd("set nospell")
     --     end,
     -- },
+    {
+        "folke/noice.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        config = function()
+            require("noice").setup({})
+        end,
+    },
 })
