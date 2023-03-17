53f53b230c8c9a22ba30984050f4b776f1ac5820
Mint <463785757@qq.com>
2023-03-17 21:48:48 +0800

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
+local act = wezterm.action
 
 wezterm.on("gui-startup", function(cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
@@ -10,4 +11,10 @@ return {
     color_scheme = "Builtin Solarized Dark",
     font = wezterm.font("Hack Nerd Font", { weight = "Regular" }),
     font_size = 14,
+    keys = {
+        { key = "LeftArrow", mods = "CMD", action = act.ActivateTabRelative(-1) },
+        { key = "RightArrow", mods = "CMD", action = act.ActivateTabRelative(1) },
+        { key = "LeftArrow", mods = "OPT", action = act.SendString("\x1bb") },
+        { key = "RightArrow", mods = "OPT", action = act.SendString("\x1bf") },
+    },
 }
