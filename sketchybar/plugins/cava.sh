deac366952627361756e1a7889f2e2f61de2b5c1
Mint <463785757@qq.com>
2023-06-20 21:05:04 +0800

    retry

--- sketchybar/plugins/cava.sh
+++ sketchybar/plugins/cava.sh
@@ -6,6 +6,11 @@
 SKETCHBAR_BIN="/opt/homebrew/bin/sketchy_bottombar"
 PLUGIN_DIR="$HOME/.config/sketchybar/plugins"
 
-cava -p $PLUGIN_DIR/cava.conf | sed -u 's/ //g; s/0/▁/g; s/1/▂/g; s/2/▃/g; s/3/▄/g; s/4/▅/g; s/5/▆/g; s/6/▇/g; s/7/█/g; s/8/█/g' | while read line; do
-  $SKETCHBAR_BIN --set $NAME label=$line
+while true
+do
+    cava -p $PLUGIN_DIR/cava.conf | sed -u 's/ //g; s/0/▁/g; s/1/▂/g; s/2/▃/g; s/3/▄/g; s/4/▅/g; s/5/▆/g; s/6/▇/g; s/7/█/g; s/8/█/g' | while read line; do
+      $SKETCHBAR_BIN --set $NAME label=$line
+    done
+
+    sleep 5
 done
