b37f5861bd9fd6bdb96f6733c6a4087c76ef39b3
Mint <463785757@qq.com>
2023-06-22 17:50:07 +0800

    fix utf8

--- sketchybar/plugins/window_title.sh
+++ sketchybar/plugins/window_title.sh
@@ -9,7 +9,7 @@ if [[ $WINDOW_TITLE = "" ]]; then
 fi
 
 if [[ ${#WINDOW_TITLE} -gt 50 ]]; then
-  WINDOW_TITLE="$(echo "$WINDOW_TITLE" | cut -c 1-50)..."
+  WINDOW_TITLE="$(echo "$WINDOW_TITLE" | cut -c 1-50 | iconv -c)..."
 fi
 
 $SKETCHBAR_BIN --set $NAME label="$WINDOW_TITLE"
