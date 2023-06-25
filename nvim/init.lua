e3e033819c9a1c49ea0a1a1776dd8bca06f7526c
Mint <463785757@qq.com>
2023-06-25 19:59:22 +0800

    fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,9 +46,18 @@ require("lazy").setup({
         config = function()
             require("monokai-pro").setup({
                 transparent_background = false,
-                terminal_colors = false,
+                terminal_colors = true,
                 devicons = true, -- highlight the icons of `nvim-web-devicons`
-                italic_comments = true,
+                styles = {
+                    comment = { italic = true },
+                    keyword = { italic = true }, -- any other keyword
+                    type = { italic = true }, -- (preferred) int, long, char, etc
+                    storageclass = { italic = true }, -- static, register, volatile, etc
+                    structure = { italic = true }, -- struct, union, enum, etc
+                    parameter = { italic = true }, -- parameter pass in function
+                    annotation = { italic = true },
+                    tag_attribute = { italic = true }, -- attribute of tag in reactjs
+                },
                 filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
                 -- Enable this will disable filter option
                 day_night = {
@@ -58,12 +67,16 @@ require("lazy").setup({
                 },
                 inc_search = "background", -- underline | background
                 background_clear = {
-                    "float_win",
+                    -- "float_win",
                     "toggleterm",
                     "telescope",
-                    "which-key",
+                    -- "which-key",
                     "renamer",
-                }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer"
+                    "notify",
+                    -- "nvim-tree",
+                    -- "neo-tree",
+                    -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+                }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
                 plugins = {
                     bufferline = {
                         underline_selected = false,
@@ -71,6 +84,7 @@ require("lazy").setup({
                     },
                     indent_blankline = {
                         context_highlight = "pro", -- default | pro
+                        context_start_underline = false,
                     },
                 },
                 ---@param c Colorscheme
@@ -1004,6 +1018,7 @@ require("lazy").setup({
     },
     {
         "j-hui/fidget.nvim",
+        branch = "legacy",
         config = function()
             require("fidget").setup({})
         end,
