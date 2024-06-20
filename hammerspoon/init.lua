3b2eaf2fec80864f94e3abb0aff2e4965524359d
Mint <463785757@qq.com>
2024-06-20 09:07:37 +0800

    update ham

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,15 +7,15 @@ hs.hotkey.bind({ "cmd" }, "2", function()
 end)
 
 hs.hotkey.bind({ "cmd" }, "3", function()
-    hs.application.launchOrFocus("Visual Studio Code")
-end)
-
-hs.hotkey.bind({ "cmd" }, "4", function()
     hs.application.launchOrFocus("Wechat")
 end)
 
-hs.hotkey.bind({ "cmd" }, "5", function()
+hs.hotkey.bind({ "cmd" }, "4", function()
     hs.application.launchOrFocus("Lark")
+end)
+
+hs.hotkey.bind({ "cmd" }, "5", function()
+    hs.application.launchOrFocus("Visual Studio Code")
 end)
 
 hs.hotkey.bind({ "ctrl" }, "1", function()
